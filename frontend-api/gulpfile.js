4c6b9ad8aaee4d41818a8aa636085a9b05f933b2
jpb <jpb.06@outlook.com>
2019-02-26 15:33:35 +0100

    Setting up distinct configs depending on environment

--- frontend-api/gulpfile.js
+++ frontend-api/gulpfile.js
@@ -1,13 +1,22 @@
-﻿/// <binding BeforeBuild='clean' Clean='clean' />
+﻿/// <binding BeforeBuild='clean, use-dev-config' Clean='clean' />
 const gulp = require('gulp');
 
 const cleanDist = require('./project-apparatus/tasks/clean.dist.task.js');
 const tscBuild = require('./project-apparatus/tasks/tsc.build.task.js');
+const configSwitcher = require('./project-apparatus/tasks/config-switcher.js');
 
 gulp.task('clean', async () => {
     await cleanDist();
 });
 
-gulp.task('build', async () => {
-    await tscBuild(true);
+gulp.task('use-dev-config', async () => {
+    await configSwitcher.useDevConfig();
+});
+
+gulp.task('build-dev', async () => {
+    await tscBuild(false, true);
+});
+
+gulp.task('build-release', async () => {
+    await tscBuild(true, false);
 });
---
+++ frontend-api/project-apparatus/tasks/config-switcher.js
@@ -0,0 +1,23 @@
+﻿const fs = require('fs-extra');
+
+const consoleUtil = require('./../util/console.util.js');
+
+var main = {};
+
+main.useDevConfig = async function () {
+    consoleUtil.printHeader('Switching to dev config ...');
+
+    await fs.copy('./src/config/dev.config.json', './dist/js/config/current.config.json');
+
+    console.log('Done.');
+};
+
+main.useProdConfig = async function () {
+    consoleUtil.printHeader('Switching to dev config ...');
+
+    await fs.copy('./src/config/prod.config.json', './dist/js/config/current.config.json');
+
+    console.log('Done.');
+};
+
+module.exports = main;

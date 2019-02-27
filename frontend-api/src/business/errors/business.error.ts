378f6b851d5ea5218ba3ea57637d4aac7b14297e
jpb <jpb.06@outlook.com>
2019-02-27 17:37:24 +0100

    Adding some custom errors for the business layer

---
+++ frontend-api/src/business/errors/business.error.ts
@@ -0,0 +1,28 @@
+﻿export enum ErrorCode {
+    // ---------------------------------------------------------------
+    // - Users
+    // ---------------------------------------------------------------
+    EmailAlreadyExists = 1,
+    EmailIsNotUnique = 2,
+    NoUserForEmail = 3,
+    UnableToPersistRole = 4,
+    UnableToPersistUser = 5,
+    // ---------------------------------------------------------------
+    // - Vacations
+    // ---------------------------------------------------------------
+}
+
+export class BusinessError extends Error {
+    errorCode: ErrorCode;
+
+    constructor(
+        message: string,
+        code: ErrorCode
+    ) {
+        super(message);
+        this.errorCode = code;
+
+        // Set the prototype explicitly.
+        Object.setPrototypeOf(this, BusinessError.prototype);
+    }
+}

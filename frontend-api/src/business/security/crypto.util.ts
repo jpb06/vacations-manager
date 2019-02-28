08df800327571f3c25376b97b0c34ae0bda87bd0
jpb <jpb.06@outlook.com>
2019-02-28 19:07:39 +0100

    Adding logic for hashing/verifying hashes

---
+++ frontend-api/src/business/security/crypto.util.ts
@@ -0,0 +1,20 @@
+﻿import * as bcrypt from 'bcrypt';
+
+export async function hash(
+    data: string
+): Promise<string> {
+    let salt = await bcrypt.genSalt(12);
+    let hash = await bcrypt.hash(data, salt);
+
+    return hash;
+}
+
+export async function verify(
+    data: string,
+    hash: string
+): Promise<boolean> {
+
+    let result = await bcrypt.compare(data, hash);
+
+    return result;
+}

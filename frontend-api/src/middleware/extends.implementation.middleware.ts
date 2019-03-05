b5c306ca63e91354b29a7d9c41d2248be861f36b
jpb <jpb.06@outlook.com>
2019-03-05 19:22:18 +0100

    Adding an express response extension to send business errors

--- frontend-api/src/middleware/extends.implementation.middleware.ts
+++ frontend-api/src/middleware/extends.implementation.middleware.ts
@@ -1,4 +1,5 @@
 ﻿import { Request, Response, NextFunction } from 'express';
+import { BusinessError, ErrorCode } from '../business/errors/business.error';
 
 export function extendsImplementation(
     req: Request,
@@ -24,13 +25,26 @@ export function extendsImplementation(
     // ----------------------------------------------------------------------------------------------------------------
 
     // Function to generate a generic response 
-    res.answer = function (
+    res.sendGenericError = function (
         status: number,
         message: string
     ): Response {
         return res.status(status).json({
             status: status,
             message: message
+        });
+    }
+
+    // Function to generate a generic response from a BusinessError
+    res.sendBusinessError = function (
+        status: number, 
+        message: string,
+        code: ErrorCode
+    ): Response {
+        return res.status(500).json({
+            status: status,
+            message: message,
+            code: code
         });
     }
 
